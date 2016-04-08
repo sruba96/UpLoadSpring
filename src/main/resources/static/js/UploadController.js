b5a1435558aa90e171619a756ae05ccda0e3e373
Paweł Malina <sruba96@gmail.com>
2016-04-08 17:01:18 +0200

    Change js file and i will change controller

--- src/main/resources/static/js/UploadController.js
+++ src/main/resources/static/js/UploadController.js
@@ -2,44 +2,50 @@
  * Created by pawel on 08.04.16.
  */
 
-var example = angular.module('UpLoad', []);
 
 
-example.controller('UpLoadController', function ($scope, $http) // serwis $http umożliwia zapytania ajax
-{
-    var self = this;
 
-    this.file = {};
+    var myApp = angular.module('myApp', []);
 
-    this.addFile = function () {
+    myApp.directive('fileModel', ['$parse', function ($parse) {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                var model = $parse(attrs.fileModel);
+                var modelSetter = model.assign;
 
-
-        var ajax = $http.post('uploadfile', this.file);
-        this.user = {};
-
-        ajax.success(function (data) {
-            if (data.result) {
-                //$scope.registerForm = true;
-            }
-        });
-    };
-
-    //Malina
-    example.directive('fileModel', ['$parse', function ($parse) {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            var model = $parse(attrs.fileModel);
-            var modelSetter = model.assign;
-
-            element.bind('change', function(){
-                scope.$apply(function(){
-                    modelSetter(scope, element[0].files[0]);
+                element.bind('change', function(){
+                    scope.$apply(function(){
+                        modelSetter(scope, element[0].files[0]);
+                    });
                 });
-            });
+            }
+        };
+    }]);
+
+    myApp.service('fileUpload', ['$http', function ($http) {
+        this.uploadFileToUrl = function(file, uploadUrl){
+            var fd = new FormData();
+            fd.append('file', file);
+            $http.post(uploadUrl, fd, {
+                    transformRequest: angular.identity,
+                    headers: {'Content-Type': undefined}
+                })
+                .success(function(){
+                })
+                .error(function(){
+                });
         }
-    };
-}]);
+    }]);
 
+    myApp.controller('myCtrl', ['$scope', 'fileUpload', function($scope, fileUpload){
 
-});
+        $scope.uploadFile = function(){
+            var file = $scope.myFile;
+            console.log('file is ' );
+            console.dir(file);
+            var uploadUrl = "/uploadfile";
+            fileUpload.uploadFileToUrl(file, uploadUrl);
+        };
+
+    }]);
